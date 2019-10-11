4db96108dc594bf02d031fe0399ba4c2c08b16f2
fsdavid <daviti1@mail.com>
2019-10-11 10:01:26 +0200

    First case for check

---
+++ test/logError.js
@@ -0,0 +1,53 @@
+const fetch = require('node-fetch')
+const nodemailer = require('nodemailer')
+require('dotenv').config()
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+})
+
+
+const logError = async (data = {}, screenShot, url = 'http://logger-pmap-service.apps-dev.hbp.eu/ivautomated.testing.error') => {
+
+    // sendEmail(data, screenShot)
+    //
+    // const response = await fetch(url, {
+    //     method: 'POST',
+    //     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    //     body: 'json=' + JSON.stringify(data)
+    // })
+    // return response
+    return null
+}
+
+const sendEmail = (data, screenShot) => {
+    transporter.sendMail({
+        from: process.env.EMAIL,
+        to: 'd.gogshelidze@fz-juelich.de',
+        subject: 'Interactive Viewer Automated test error',
+        text: 'There is error founded with Automated testing of Interactive Viewer. \n\nTest: ' + data.test + '\nCase: ' + data.case + '\nError: ' + data.error,
+        attachments: [{
+            filename: 'errorImage.png',
+            content: screenShot,
+        }]
+    }, function(error, info){
+        if (error) {
+            fetch('http://logger-pmap-service.apps-dev.hbp.eu/ivautomated.testing.email', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                body: 'json={"error": "Email not send"}'
+            })
+            console.log('Email not sent')
+            console.log(error)
+        }
+    })
+}
+
+
+
+module.exports = {logError}
