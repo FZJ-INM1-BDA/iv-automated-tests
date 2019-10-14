be2cde934d1f8c030c886aa7b05a55912be9ec17
fsdavid <daviti1@mail.com>
2019-10-14 15:49:03 +0200

    selections parcellations

--- test/template-and-parcellation-selections.spec.js
+++ test/template-and-parcellation-selections.spec.js
@@ -6,7 +6,12 @@ const globalVariables = _.pick(global, ['browser', 'expect'])
 
 let page
 
+
+
 exports.selectTAndP = async function selectTAndP() {
+
+
+
     afterEach(async function () {
         if (this.currentTest.state === 'failed') {
             const screenShot = await page.screenshot()
@@ -19,154 +24,175 @@ exports.selectTAndP = async function selectTAndP() {
         await page.close()
     })
 
-    ////////////////// Big Brain //////////////////////
-    it('Select BigBrain (Parcellation Auto "Grey/White matter")',
-        checkRegionSelectionByTwoRegion.bind(
-            this,
-            buttonTextForParcellation = ' Big Brain (Histology) ',
-            area1Coordinates = [530, 90],
-            area1Name = 'White matter',
-            area2Coordinates = [660, 100],
-            area2Name = 'Grey matter',
-            waitAfterPageLoads = 4000
-        )
-    )
-
-    it('Select BigBrain (Parcellation Cytoarchitectonic Maps)',
-        checkRegionSelectionByTwoRegion.bind(
-            this,
-            buttonTextForParcellation = ' Cytoarchitectonic Maps ',
-            area1Coordinates = [340, 180],
-            area1Name = 'Area TE 1.0 (HESCHL) (interpolated)',
-            area2Coordinates = [630, 60],
-            area2Name = 'Area 6d1 (PreCG) (interpolated)',
-            waitAfterPageLoads = 3000
-        )
-    )
-
-    it('Select BigBrain (Parcellation BigBrain Cortical Layers Segmentation)',
-        checkRegionSelectionByTwoRegion.bind(
-            this,
-            buttonTextForParcellation = ' BigBrain Cortical Layers Segmentation ',
-            area1Coordinates = [320, 245],
-            area1Name = 'cortical layer 6',
-            area2Coordinates = [500, 250],
-            area2Name = 'cortical layer 1',
-            waitAfterPageLoads = 3000
-        )
-    )
+    //////////////// Big Brain //////////////////////
+    // it('Select BigBrain (Parcellation Auto "Cytoarchitectonic Maps")',
+    //     async function() {
+    //         page = await browser.newPage()
+    //         await checkRegionSelectionByTwoRegion(
+    //             buttonTextForParcellation = ' Big Brain (Histology) ',
+    //             area1Coordinates = [340, 180],
+    //             area1Name = 'Area TE 1.0',
+    //             area2Coordinates = [630, 60],
+    //             area2Name = 'Area 6d1',
+    //             waitAfterPageLoads = 3000
+    //         )
+    //     }
+    // )
+    //
+    // it('Select BigBrain (Parcellation Grey/White matter)',
+    //     async function() {
+    //         page = await browser.newPage()
+    //         await checkRegionSelectionByTwoRegion(
+    //             buttonTextForParcellation = ' Grey/White matter ',
+    //             area1Coordinates = [530, 90],
+    //             area1Name = 'White matter',
+    //             area2Coordinates = [660, 100],
+    //             area2Name = 'Grey matter',
+    //             waitAfterPageLoads = 4000)
+    //     }
+    //
+    // )
+    //
+    // it('Select BigBrain (Parcellation BigBrain Cortical Layers Segmentation)',
+    //     async function() {
+    //         page = await browser.newPage()
+    //         await checkRegionSelectionByTwoRegion(
+    //             this,
+    //             buttonTextForParcellation = ' BigBrain Cortical Layers Segmentation ',
+    //             area1Coordinates = [320, 245],
+    //             area1Name = 'cortical layer 6',
+    //             area2Coordinates = [500, 250],
+    //             area2Name = 'cortical layer 1',
+    //             waitAfterPageLoads = 3000
+    //         )
+    //     }
+    // )
 
     /////////////////// MNI Colin 27 ///////////////////
     it('Select MNI Colin 27 (Parcellation JuBrain Cytoarchitectonic Atlas',
-        checkRegionSelectionByTwoRegion.bind(
-            this,
-            buttonTextForParcellation = ' MNI Colin 27 ',
-            area1Coordinates = [550, 150],
-            area1Name = 'Area 7M (SPL) - left hemisphere',
-            area2Coordinates = [540, 550],
-            area2Name = 'Area p32 (pACC) - left hemisphere',
-            waitAfterPageLoads = 2000
-        )
+        async function() {
+            page = await browser.newPage()
+            await checkRegionSelectionByTwoRegion(
+                buttonTextForParcellation = ' MNI Colin 27 ',
+                area1Coordinates = [550, 150],
+                area1Name = 'Area 7M (SPL) - left hemisphere',
+                area2Coordinates = [540, 550],
+                area2Name = 'Area p32 (pACC) - left hemisphere',
+                waitAfterPageLoads = 2000
+            )
+        }
     )
 
     /////////////////// ICBM 2009c Nonlinear Asymmetric ///////////////////
     it('Select ICBM 2009c Nonlinear Asymmetric (Parcellation JuBrain Cytoarchitectonic Atlas)',
-        checkRegionSelectionByTwoRegion.bind(
-            this,
-            buttonTextForParcellation = ' ICBM 2009c Nonlinear Asymmetric ',
-            area1Coordinates = [550, 270],
-            area1Name = 'Fastigial Nucleus (Cerebellum) - right hemisphere',
-            area2Coordinates = [600, 490],
-            area2Name = 'Area 6ma (preSMA, mesial SFG) - left hemisphere',
-            waitAfterPageLoads = 2000
-        )
+        async function() {
+            page = await browser.newPage()
+            await checkRegionSelectionByTwoRegion(
+                buttonTextForParcellation = ' ICBM 2009c Nonlinear Asymmetric ',
+                area1Coordinates = [550, 270],
+                area1Name = 'Fastigial Nucleus (Cerebellum) - right hemisphere',
+                area2Coordinates = [600, 490],
+                area2Name = 'Area 6ma (preSMA, mesial SFG) - left hemisphere',
+                waitAfterPageLoads = 2000
+            )
+        }
     )
 
     it('Select ICBM 2009c Nonlinear Asymmetric (Parcellation Fibre Bundle Atlas - Long Bundle)',
-        checkRegionSelectionByTwoRegion.bind(
-            this,
-            buttonTextForParcellation = ' Fibre Bundle Atlas - Long Bundle ',
-            area1Coordinates = [300, 210],
-            area1Name = 'InferiorFrontoOccipital - Right',
-            area2Coordinates = [680, 590],
-            area2Name = 'InferiorLongitudinal - Left',
-            waitAfterPageLoads = 2000
-        )
+        async function() {
+            page = await browser.newPage()
+            await checkRegionSelectionByTwoRegion(
+                this,
+                buttonTextForParcellation = ' Fibre Bundle Atlas - Long Bundle ',
+                area1Coordinates = [300, 210],
+                area1Name = 'InferiorFrontoOccipital - Right',
+                area2Coordinates = [680, 590],
+                area2Name = 'InferiorLongitudinal - Left',
+                waitAfterPageLoads = 2000
+            )
+        }
     )
 
     it('Select ICBM 2009c Nonlinear Asymmetric (Parcellation Fibre Bundle Atlas - Short Bundle)',
-        checkRegionSelectionByTwoRegion.bind(
-            this,
-            buttonTextForParcellation = ' Fibre Bundle Atlas - Short Bundle ',
-            area1Coordinates = [300, 100],
-            area1Name = 'rh_SP-SM_0',
-            area2Coordinates = [640, 540],
-            area2Name = 'lh_PoCi-PrCu_0',
-            waitAfterPageLoads = 2000
-        )
+        async function() {
+            page = await browser.newPage()
+            await checkRegionSelectionByTwoRegion(
+                buttonTextForParcellation = ' Fibre Bundle Atlas - Short Bundle ',
+                area1Coordinates = [300, 100],
+                area1Name = 'rh_SP-SM_0',
+                area2Coordinates = [640, 540],
+                area2Name = 'lh_PoCi-PrCu_0',
+                waitAfterPageLoads = 2000
+            )
+        }
     )
 
     /////////////// Waxholm Space rat brain MRI/DTI ///////////////////
     it('Select Waxholm Space rat brain MRI/DTI (Parcellation Waxholm Space rat brain atlas v3 )',
-        checkRegionSelectionByTwoRegion.bind(
-            this,
-            buttonTextForParcellation = ' Waxholm Space rat brain MRI/DTI ',
-            area1Coordinates = [350, 170],
-            area1Name = 'neocortex',
-            area2Coordinates = [320, 560],
-            area2Name = 'corpus callosum and associated subcortical white matter',
-            waitAfterPageLoads = 3000
-        )
+        async function() {
+            page = await browser.newPage()
+            await checkRegionSelectionByTwoRegion(
+                buttonTextForParcellation = ' Waxholm Space rat brain MRI/DTI ',
+                area1Coordinates = [350, 170],
+                area1Name = 'neocortex',
+                area2Coordinates = [320, 560],
+                area2Name = 'corpus callosum and associated subcortical white matter',
+                waitAfterPageLoads = 3000)
+        }
     )
 
-    async function checkRegionSelectionByTwoRegion() {
-        page = await browser.newPage()
-        await page.setViewport({width: 800, height: 798, deviceScaleFactor: 1})
-        // ToDo Implemnt {waitUntil: 'networkidle2'} when it will be implemented
-        await page.goto('https://interactive-viewer-next.apps-dev.hbp.eu/', {waitUntil: 'networkidle2'})
-        await page.waitFor(waitAfterPageLoads)
 
-        // Choose ICBM 2009c Nonlinear Asymmetric ( JuBrain Cytoarchitectonic Atlas )
-        const TemplateXPath = "//*[(text() = '" +  buttonTextForParcellation + "' or . = '" + buttonTextForParcellation + "')]"
-        await page.waitForXPath(TemplateXPath, 5000)
-        const [templateDefined] = await page.$x(TemplateXPath)
-        if (templateDefined) templateDefined.click()
-
-        await page.waitFor(waitAfterPageLoads)
-
-        await page.mouse.move(area1Coordinates[0], area1Coordinates[1])
-        await page.waitFor(2000)
-
-        // Cursor should be on White metter
-        const regionHoverTextXPath1 = "//*[text()[contains(.,'" + area1Name + "')]]"
-        await page.waitForXPath(regionHoverTextXPath1, 5000)
-        await page.waitFor(1000)
-        const [hoverTextCheck1] = await page.$x(regionHoverTextXPath1)
-        if (hoverTextCheck1) {
-            await page.waitFor(500)
-            const TextOfHoverTextCheck1 = await page.evaluate(element => element.textContent, hoverTextCheck1)
-            expect(TextOfHoverTextCheck1.trim()).to.equal(area1Name)
-        }
-
-        await page.waitFor(500)
-
-        // await page.mouse.move(500, 250)
-        await page.mouse.move(area2Coordinates[0], area2Coordinates[1])
-        await page.waitFor(500)
-
-        // Cursor should be on White metter
-        const regionHoverTextXPath2 = "//*[text()[contains(.,'" + area2Name + "')]]"
-        await page.waitForXPath(regionHoverTextXPath2, 5000)
-        await page.waitFor(1000)
-        const [hoverTextCheck2] = await page.$x(regionHoverTextXPath2)
-        if (hoverTextCheck2) {
-            await page.waitFor(500)
-            const TextOfHoverTextCheck2 = await page.evaluate(element => element.textContent, hoverTextCheck2)
-            expect(TextOfHoverTextCheck2.trim()).to.equal(area2Name)
-        }
-    }
 
 }
+
+
+const checkRegionSelectionByTwoRegion = async (buttonTextForParcellation, area1Coordinates, area1Name, area2Coordinates, area2Name, waitAfterPageLoads) => {
+    // page = await browser.newPage()
+    await page.setViewport({width: 800, height: 798, deviceScaleFactor: 1})
+    // ToDo Implemnt {waitUntil: 'networkidle2'} when it will be implemented
+    await page.goto('https://interactive-viewer-next.apps-dev.hbp.eu/', {waitUntil: 'networkidle2'})
+    await page.waitFor(waitAfterPageLoads)
+
+    // Choose ICBM 2009c Nonlinear Asymmetric ( JuBrain Cytoarchitectonic Atlas )
+    const TemplateXPath = "//*[(text() = '" +  buttonTextForParcellation + "' or . = '" + buttonTextForParcellation + "')]"
+    await page.waitForXPath(TemplateXPath, 5000)
+    const [templateDefined] = await page.$x(TemplateXPath)
+    if (templateDefined) templateDefined.click()
+
+    await page.waitFor(waitAfterPageLoads)
+
+    await page.mouse.move(area1Coordinates[0], area1Coordinates[1])
+    await page.waitFor(2000)
+
+    // Cursor should be on White metter
+    const regionHoverTextXPath1 = "//*[text()[contains(.,'" + area1Name + "')]]"
+    await page.waitForXPath(regionHoverTextXPath1, 5000)
+    await page.waitFor(1000)
+    const [hoverTextCheck1] = await page.$x(regionHoverTextXPath1)
+    if (hoverTextCheck1) {
+        await page.waitFor(500)
+        const TextOfHoverTextCheck1 = await page.evaluate(element => element.textContent, hoverTextCheck1)
+        expect(TextOfHoverTextCheck1.trim()).to.equal(area1Name)
+    }
+
+    await page.waitFor(500)
+
+    // await page.mouse.move(500, 250)
+    await page.mouse.move(area2Coordinates[0], area2Coordinates[1])
+    await page.waitFor(500)
+
+    // Cursor should be on White metter
+    const regionHoverTextXPath2 = "//*[text()[contains(.,'" + area2Name + "')]]"
+    await page.waitForXPath(regionHoverTextXPath2, 5000)
+    await page.waitFor(1000)
+    const [hoverTextCheck2] = await page.$x(regionHoverTextXPath2)
+    if (hoverTextCheck2) {
+        await page.waitFor(500)
+        const TextOfHoverTextCheck2 = await page.evaluate(element => element.textContent, hoverTextCheck2)
+        expect(TextOfHoverTextCheck2.trim()).to.equal(area2Name)
+    }
+}
+
 
 ////////////////// Big Brain //////////////////////
 // it('Select BigBrain (Parcellation Auto "Grey/White matter")', async function () {
